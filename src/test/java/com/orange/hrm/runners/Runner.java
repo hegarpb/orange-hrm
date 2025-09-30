45510895b7322db28adeeb8323587c010cc08c79
hegarpb <hegaregayz@gmail.com>
2025-09-30 09:06:34 +0700

    commit

--- src/test/java/com/orange/hrm/runners/Runner.java
+++ src/test/java/com/orange/hrm/runners/Runner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {
-                "src/test/resources/features/pim/AddEmployee.feature"
+                "src/test/resources/features/pim"
 
 }, glue = {
                 "com.orange.hrm",
